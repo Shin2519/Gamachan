0138aec1fd1a4332ab51d4ced20b5be5a46ed8fb
COSM1C-0715 <tatsuhiro.tkn@gmail.com>
2025-10-07 03:00:17 +0900

    とりあえず自分でマウスカーソルらしきもののテクスチャをペイントで2種類作って、クリックしている時とそうでない時でImageを変えられるようにした。

--- Assets/Scripts/playercontroll.cs
+++ Assets/Scripts/playercontroll.cs
@@ -4,17 +4,20 @@ using UnityEngine.InputSystem;
 
 public class playercontroll : MonoBehaviour
 {
+    public static playercontroll instance;
     [SerializeField]
     GameObject cube;
-    Actions action;
     [SerializeField]
     private Image moucecursor;
+    [SerializeField,Header("マウスカーソルの種類")]
+    private Sprite[] moucecursorSprite;
     [SerializeField]
     private RectTransform cursor_position;
     [SerializeField]
     Vector2 MovInput;
     [SerializeField]
     private bool IsInter;
+    public Vector2 MoucePos => MovInput;
     private void OnMove(InputValue val)
     {
         MovInput = val.Get<Vector2>();
@@ -24,10 +27,17 @@ public class playercontroll : MonoBehaviour
     {
         IsInter = val.isPressed;
     }
+
+    void Awake()
+    {
+        instance = this;
+        moucecursor = GetComponent<Image>();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Cursor.visible = false;
+        moucecursor.sprite = moucecursorSprite[0];
     }
 
     // Update is called once per frame
@@ -36,14 +46,22 @@ public class playercontroll : MonoBehaviour
         Vector2 pos;
         pos = MovInput;
         cursor_position.position = pos;
+        Vector3 CurrentPos = cube.transform.position;
         if(IsInter)
         {
+            moucecursor.sprite = moucecursorSprite[1];
             Ray ray = Camera.main.ScreenPointToRay(MovInput);
             RaycastHit hit;
             if(Physics.Raycast(ray,out hit))
             {
-                cube.transform.position = new Vector3(MovInput.x,MovInput.y,-50.0f);
+                Vector3 TargetPos = new Vector3(MovInput.x, MovInput.y, -50.0f);
+                cube.transform.position = TargetPos;
+                Vector3 DisPos = TargetPos - CurrentPos;
             }
+        }
+        else
+        {
+            moucecursor.sprite = moucecursorSprite[0];
         }
     }
 }
