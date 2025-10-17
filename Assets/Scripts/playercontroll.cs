5e3a190e17c829ca28c841cc9e62f069fd1e66fd
COSM1C-0715 <tatsuhiro.tkn@gmail.com>
2025-10-17 19:45:32 +0900

    振る判定はできた

--- Assets/Scripts/playercontroll.cs
+++ Assets/Scripts/playercontroll.cs
@@ -7,18 +7,26 @@ using UnityEngine.EventSystems;
 public class playercontroll : MonoBehaviour
 {
     public static playercontroll instance;
-
+    enum STATE 
+    { 
+        normal,
+        shake
+    }
+    [SerializeField]
+    private STATE state;
+    [SerializeField,Header("振れているかどうか判定する範囲")]
+    private float judge;
     [SerializeField,Header("マウスカーソルのポジション")]
     private RectTransform cursor_position;
     [SerializeField,Header("カーソルを動かしたときに代入される入力ベクトル")]
     Vector2 MovInput;
     [SerializeField]
     private EventSystem E_System;
-    [SerializeField,Header("判定するレイヤー")]
-    private LayerMask layer;
-    string layername;
     [SerializeField]
-    GraphicRaycaster G_raycas;
+    GraphicRaycaster G_raycast;
+    [SerializeField]
+    private RectTransform ParentCanvas;
+    GameObject ui;
     private void OnMove(InputValue val)
     {
         MovInput = val.Get<Vector2>();
@@ -32,6 +40,7 @@ public class playercontroll : MonoBehaviour
     void Start()
     {
         Cursor.visible = false;//マウスカーソルを非表示にする
+        state = STATE.normal;
     }
 
     // Update is called once per frame
@@ -55,7 +64,7 @@ public class playercontroll : MonoBehaviour
         }
         else
         {
-            
+            ui = null;
         } 
     }
 
@@ -64,10 +73,33 @@ public class playercontroll : MonoBehaviour
         PointerEventData data = new PointerEventData(E_System);
         data.position = MovInput;
         List<RaycastResult> results = new List<RaycastResult>();
-        G_raycas.Raycast(data, results);
-        foreach(var result in results)
+        G_raycast.Raycast(data, results);
+        if(results.Count > 0)
         {
-            result.gameObject.transform.position = data.position;
+            ui = results[0].gameObject;
         }
+        if (ui!= null)
+        {
+            int Count = 1;
+            RectTransform ui_Pos = ui.GetComponent<RectTransform>();
+            Vector2 CurrentPos = ui_Pos.position;
+            Vector2 localPoint;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(ParentCanvas, MovInput, null, out localPoint);
+            ui_Pos.anchoredPosition = localPoint;
+            Vector2 AfterPos = ui_Pos.position;
+            Vector2 Dis = AfterPos - CurrentPos;
+            if(Dis.magnitude > judge&&Count!=0)
+            {
+                Count = 0;
+                state = STATE.shake;
+                int rnd = Random.Range(0, 2);
+                Debug.Log(rnd);
+            }
+            else if(Dis.magnitude < 0.1f)
+            {
+                state = STATE.normal;
+            }
+        }
+        
     }
 }
