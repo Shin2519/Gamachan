a3e42649715f065b96da78d61fdb87745727eafe
5F\ktc <>
2025-11-14 19:40:38 +0900

    audioのついか

--- Assets/Scripts/PriceManager/RuleCheckScene.cs
+++ Assets/Scripts/PriceManager/RuleCheckScene.cs
@@ -3,18 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class RuleCheckScene : MonoBehaviour
 {
+    [SerializeField] private AudioSource audiosource;
+    [SerializeField] private AudioClip clip;
+    [SerializeField] private AudioClip clip2;
+
+    private void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
     public void RuleYes()
     {
         SceneManager.LoadScene("RuleScene");
+        audiosource.PlayOneShot(clip);
     }
 
     public void RuleNo()
     {
         SceneManager.LoadScene("ModeSelectScene");
+        audiosource.PlayOneShot(clip);
     }
 
     public void Title()
     {
         SceneManager.LoadScene("TitleScene");
+        audiosource.PlayOneShot(clip2);
     }
 }
--- Assets/Scripts/PriceManager/RuleScene.cs
+++ Assets/Scripts/PriceManager/RuleScene.cs
@@ -5,12 +5,15 @@ public class RuleScene : MonoBehaviour
     [SerializeField] private GameObject Page1;
     [SerializeField] private GameObject Page2;
     [SerializeField] private GameObject Page3;
+    private AudioSource audiosource;
+    [SerializeField] private AudioClip clip;
 
     private void Start()
     {
         Page1.SetActive(true);
         Page2.SetActive(false);
         Page3.SetActive(false);
+        audiosource = GetComponent<AudioSource>();
     }
 
 
@@ -27,23 +30,27 @@ public class RuleScene : MonoBehaviour
     {
         Page1.SetActive(false);
         Page2.SetActive(true);
+        audiosource.PlayOneShot(clip);
     }
 
     public void Page2to1()
     {
         Page1.SetActive(true);
         Page2.SetActive(false);
+        audiosource.PlayOneShot(clip);
     }
 
     public void Page2to3()
     {
         Page2.SetActive(false);
         Page3.SetActive(true);
+        audiosource.PlayOneShot(clip);
     }
 
     public void Page3to2()
     {
         Page2.SetActive(true);
         Page3.SetActive(false);
+        audiosource.PlayOneShot(clip);
     }
 }
