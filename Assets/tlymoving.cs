a6609a4c6dbb382911177f9573ba1216a69e12c3
COSM1C-0715 <tatsuhiro.tkn@gmail.com>
2025-11-21 19:45:46 +0900

    今日はこれで終わり

---
+++ Assets/tlymoving.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class tlymoving : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    Vector2 leftPos = new Vector2(-700f,-300f);
+    Vector2 rightPos = new Vector2(700f,-300f);
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RectTransform tlyPos = GetComponent<RectTransform>();
+        Vector2 Pos = tlyPos.anchoredPosition;
+        Pos.x += speed * Time.deltaTime;
+    }
+}
