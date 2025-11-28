4cecc1f55b4f1f32429cc988c1931cf5224b2c8d
5F\ktc <>
2025-11-28 15:23:04 +0900

    数値ランダム

--- Assets/Scripts/PriceManager/Selectimage.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Selectimage", menuName = "Scriptable Objects/Selectimage")]
-public class Selectimage : ScriptableObject
-{
-    [SerializeField] public Sprite image;
-
-}
