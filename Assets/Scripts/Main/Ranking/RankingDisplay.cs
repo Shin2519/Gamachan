9bd8b9bcd2dc0f98d1e663c21216f19765c78671
COSM1C-0715 <tatsuhiro.tkn@gmail.com>
2026-06-06 16:35:36 +0900

    ランキングのスクリプトについて見直し

--- Assets/Scripts/Main/Ranking/RankingDisplay.cs
+++ Assets/Scripts/Main/Ranking/RankingDisplay.cs
@@ -10,6 +10,7 @@ public class RankingDisplay : MonoBehaviour
         public int PlayerScore;
     }
 
+    static int[] RankingScore = new int[5];
     RankingProfile profile;
 
     List<RankingProfile> RankingProfiles = new List<RankingProfile>();
@@ -45,39 +46,39 @@ public class RankingDisplay : MonoBehaviour
     {
         for(int i = 0;i < 5;i++)
         {
-            profile.PlayerName = PlayerPrefs.GetString((i + 1).ToString(),"");
+            profile.PlayerName = PlayerPrefs.GetString((i + 1).ToString(),string.Empty);
 
             profile.PlayerScore = PlayerPrefs.GetInt((i + 1).ToString(),0);
 
             RankingProfiles.Add(profile);
         }
     }
-
     public static int RankingJudge(int New_Score)
     {
-        int RankingNum =0;
-        for (int i = 0;i < 5;i++)
+        int rankingNum =0;
+        for (int i = 0;i < RankingScore.Length;i++)
         {
-            if(!PlayerPrefs.HasKey("Score" + (i + 1).ToString()))
+                rankingNum = i + 1;
+            if(!PlayerPrefs.HasKey(string.Format("Score{1}", rankingNum)))
             {
-                RankingNum = i + 1;
-                PlayerPrefs.SetInt("Score" + RankingNum.ToString(),New_Score);
+                PlayerPrefs.SetInt(string.Format( "Score{1}" , rankingNum),New_Score);
                 break;
             }
         }
 
-        List<int> RankingScore = new List<int>();
+        
 
-        for(int i = 0;i < 5;i++)
+        for(int i = 0;i < RankingScore.Length;i++)
         {
-            RankingScore.Add(PlayerPrefs.GetInt("Score" + (i + 1).ToString()));
+            rankingNum = i + 1;
+            RankingScore[i] = PlayerPrefs.GetInt(string.Format("Score{1}", rankingNum));
         }
 
         if (RankingScore[5]<=New_Score)
         {
-            RankingNum = 5;
-            PlayerPrefs.SetInt("Score" + RankingNum.ToString(), New_Score);
+            rankingNum = 5;
+            PlayerPrefs.SetInt(string.Format("Score{1}", rankingNum), New_Score);
         }
-        return RankingNum;
+        return rankingNum;
     }
 }
