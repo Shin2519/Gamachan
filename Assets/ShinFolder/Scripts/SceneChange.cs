36929f3eefcad20a4fc6e151bd727a2e670de52b
DESKTOP-FFHH66U\ktc <>
2026-06-12 19:44:59 +0900

    Revert "Credit Scene"
    This reverts commit 0be4be719e14abb30ba7e3913809cb67645539a3.

--- Assets/ShinFolder/Scripts/SceneChange.cs
+++ Assets/ShinFolder/Scripts/SceneChange.cs
@@ -1,4 +1,4 @@
-ï»¿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
@@ -9,18 +9,10 @@ public class SceneChange : MonoBehaviour
     [SerializeField]
     private Sound sound;
     
-    [Header("ã‚ªãƒ—ã‚·ãƒ§ãƒ³ãƒ‘ãƒãƒ«")]
+    [Header("ƒIƒvƒVƒ‡ƒ“ƒpƒlƒ‹")]
     public GameObject optionPanel;
 
     [SerializeField] private Scene[] scene;
-
-    public GameObject Canvace1;
-    public GameObject Canvace2;
-
-    private void Start()
-    {
-        
-    }
 
     public void ChangeButton_st()
     {
@@ -46,17 +38,7 @@ public class SceneChange : MonoBehaviour
     {
         FadeManager.Instance.LoadLevel("ChallengeModeScene", 1.0f, null, null);
     }
-
-    public void ChangeButton_cr1()
-    {
-
-    }
-
-    public void ChangeButton_cr2()
-    {
-
-    }
-
+    
 
     public void EndGame()
     {
